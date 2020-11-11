ba4c2e19978df15aada8ceaf726c15667309512d
Camilla Murgia <c.murgia4@studenti.uniss.it>
2020-11-11 14:39:11 +0100

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants..'
+
+Restaurant.create(name: 'ItalianRestaurant', address: 'Via Italia', phone_number: '+393474224091', category: 'italian')
+Restaurant.create(name: 'FrenchRestaurant', address: 'Rue France', phone_number: '+333474224092', category: 'french')
+Restaurant.create(name: 'ChineseRestaurant', address: 'chinese street', phone_number: '+863474224093', category: 'chinese')
+Restaurant.create(name: 'japanese', address: 'japanese street', phone_number: '+813474224094', category: 'japanese')
+Restaurant.create(name: 'belgian', address: 'Rue belgian', phone_number: '+323474224091', category: 'belgian')
+
+puts 'Restaurants created'
