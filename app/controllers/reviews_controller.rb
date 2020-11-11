265e0b6f2cdc315ceab959afeb600d4dd5e4a2ec
Camilla Murgia <c.murgia4@studenti.uniss.it>
2020-11-11 17:07:57 +0100

    added the methods in view controller and added the form on the view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  # def index
+  #   @reviews = Review.all
+  # end
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      # happy path
+      redirect_to restaurant_path(@restaurant)
+    else
+      # sad path
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
